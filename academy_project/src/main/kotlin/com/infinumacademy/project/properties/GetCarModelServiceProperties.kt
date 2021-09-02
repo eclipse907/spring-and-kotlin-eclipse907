5cf15662d169949964e1f700a46ccab6b2e03aef
Tin Reiter <56761410+eclipse907@users.noreply.github.com>
2021-09-02 14:03:22 +0200

    Fixed errors

--- academy_project/src/main/kotlin/com/infinumacademy/project/properties/GetCarModelServiceProperties.kt
+++ academy_project/src/main/kotlin/com/infinumacademy/project/properties/GetCarModelServiceProperties.kt
@@ -1,8 +1,10 @@
 package com.infinumacademy.project.properties
 
 import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.boot.context.properties.ConstructorBinding
 
 @ConfigurationProperties(prefix = "get-car-model-service")
+@ConstructorBinding
 data class GetCarModelServiceProperties(
     val baseUrl: String
 )
--- academy_project/src/main/kotlin/com/infinumacademy/project/properties/UpdateCarModelsSchedulingProperties.kt
+++ academy_project/src/main/kotlin/com/infinumacademy/project/properties/UpdateCarModelsSchedulingProperties.kt
@@ -1,9 +1,12 @@
 package com.infinumacademy.project.properties
 
 import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.boot.context.properties.ConstructorBinding
 import java.time.Duration
 
 @ConfigurationProperties(prefix = "update-car-models.scheduling")
+@ConstructorBinding
 data class UpdateCarModelsSchedulingProperties(
-    val delay: Duration
+    val delay: Duration,
+    val enabled: Boolean
 )
